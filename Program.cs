5d0f9880ae2c892fd9dcf0e88e46abc072d350e1
ekta12345 <ektakumbhare3@gmail.com>
2021-05-07 18:04:33 +0530

    Added:Elements in a stack

--- Program.cs
+++ Program.cs
@@ -6,7 +6,15 @@ namespace StackNQueue
     {
         static void Main(string[] args)
         {
-           Console.WriteLine("Stack A nd Queue Program")
+            Console.WriteLine("Stack A nd Queue Program");
+            Stack stack = new Stack(); //create object of Stack class
+            stack.Push(56); //call Push method Push element in stack 
+            stack.Push(30);//call Push method Push element in stack  
+            stack.Push(70);// call Push method Push element in stack 
+            stack.Display();//call Display method Display element in stack 
+            Console.ReadLine();
+
+
 
         }
     }
