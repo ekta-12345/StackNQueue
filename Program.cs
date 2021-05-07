7febed50c0059666fdda1f20061b04b27ceb6b04
ekta12345 <ektakumbhare3@gmail.com>
2021-05-07 17:59:18 +0530

    First commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StackNQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Stack A nd Queue Program")
+
+        }
+    }
+}
