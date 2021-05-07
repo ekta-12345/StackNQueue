bf249958465a8a187fbec8e91cc8ce81590fad3f
ekta12345 <ektakumbhare3@gmail.com>
2021-05-07 18:20:59 +0530

    Added:Ability to create a Queue

--- Program.cs
+++ Program.cs
@@ -17,6 +17,13 @@ namespace StackNQueue
             stack.Peek(); //call Peek method print top of stack 
             stack.Pop(); //call Pop method and remove top of the stack 
             stack.isEmpty(); //call isEmpty method remove all elements in stack
+            //UC3
+            Queue queue = new Queue();  //create object of Queue class
+            queue.Enqueue(56); //call method add Element in queue //UC3
+            queue.Enqueue(30); //call method add Element in queue //UC3
+            queue.Enqueue(70); //call method add Element in queue //UC3
+            queue.Display(); //call Display method Display element in Queue  
+            queue.Peek(); //call Display method Display Top of the element in Queue 
             Console.ReadLine();
 
 
